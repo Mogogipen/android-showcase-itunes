dcfddeef7513b0cede6cb8e31a3d8f3b576d33c7
nreimschiissel <nick.reimschiissel@nisc.coop>
2023-09-21 17:16:00 -0500

    base-app
    + Added layout files for the album detail screen.
    + Utilize a SlidingPaneLayout to display both the list and the detail if they can fit.

--- app/src/main/java/com/example/itunessteedpractice/generics/ProcessState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.itunessteedpractice.generics
-
-sealed class ProcessState<out T> {
-    data class Success<T>(val result: T): ProcessState<T>()
-    object Loading: ProcessState<Nothing>()
-    data class Error(val error: Throwable): ProcessState<Nothing>()
-}
--- app/src/main/java/com/example/itunessteedpractice/view/AlbumDetailFragment.kt
+++ app/src/main/java/com/example/itunessteedpractice/view/AlbumDetailFragment.kt
@@ -1,41 +1,31 @@
 package com.example.itunessteedpractice.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.itunessteedpractice.R
-import com.example.itunessteedpractice.databinding.FragmentSecondBinding
+import androidx.fragment.app.Fragment
+import com.example.itunessteedpractice.databinding.FragmentAlbumDetailBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment: Fragment() {
+class AlbumDetailFragment: Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentAlbumDetailBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = FragmentAlbumDetailBinding.inflate(inflater, container, false)
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
     }
 
     override fun onDestroyView() {
